85341e6c3ec9812f9562cc9d307fc629e980053a
mbolonzi-design <mbolonzi.design@gmail.com>
2022-09-12 18:39:25 +0300

    new files

---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contacts() {
+    return (
+        <div>
+            <h2>Get in touch</h2>
+            <p>Phone: +254 700337642</p> <br />
+            <p>Email:moviehubke@gmail.com</p>
+        </div>
+    );
+}
+
+export default Contacts;
