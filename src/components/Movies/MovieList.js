c7e6c106ea3ba3b73aeb6cc38bfdd6e45e593f5c
mbolonzi-design <mbolonzi.design@gmail.com>
2022-09-12 13:33:56 +0300

    fetch data from sinatra API

--- src/components/Movies/MovieList.js
+++ src/components/Movies/MovieList.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import './MovieList.css';
 
 function MovieList(){
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:9292/movies')
+        .then(response => response.json())
+        .then(data => setMovies(data));
+    }, []);
+
+    return (
+        <div>
+            {movies.map(movie => (
+                <div key={movie.id} className="movie-container">
+                    <h1>{movie.title}</h1>
+                    <div>
+                        {movie.plot}
+                        {movie.year} <br />
+                        {movie.rating}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 
 }
 export default MovieList
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-function Reviews(){
-   return(
-    <div>
-        <h3>Movie Reviews</h3>
-    </div>
-   )
+
+function Reviews() {
+
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:9292/reviews')
+        .then(response => response.json())
+        .then(data => setReviews(data));
+    }, []);
+
+
+    return (
+        <div>
+            {reviews.map(review => (
+                <div key={review.id} className="review-container">
+                    <h1>{review.title}</h1>
+                    <div>
+                        {review.content}
+                        {review.rating} <br />
+                        {review.movie_id}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
-
 export default Reviews;
